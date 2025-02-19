5ebafd87ef936ea4c5974282bc5763064cfc59ce
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-02-19 11:09:04 -0400

    chore(nb_iot): try to simulate all the needed stuff

--- sim/nb_iot.cc
+++ sim/nb_iot.cc
@@ -6,120 +6,250 @@
 #include <ns3/buildings-helper.h>
 #include "ns3/energy-module.h"
 #include "ns3/device-energy-model.h"
-//#include "ns3/gtk-config-store.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE("NBIoT");
 
-// Function to log remaining energy
+// ==================== LOGGING CALLBACKS ====================
 void EnergyConsumptionCallback(double oldEnergy, double newEnergy) {
-  std::cout << "Energy changed from " << oldEnergy
-      << " J to " << newEnergy
-      << " J at " << Simulator::Now().GetSeconds() << "s";
+  NS_LOG_DEBUG(Simulator::Now().GetSeconds() << "s: [ENERGY] Remaining: " 
+                << newEnergy << " J (Δ " << newEnergy - oldEnergy << " J)");
 }
 
-int main (int argc, char *argv[])
-{
-  Time simTime = MilliSeconds (1050);
-  bool useCa = true;
-  // bool useCa = false;
+void PhyTxTrace(std::string context, Ptr<const Packet> p) {
+  std::size_t nodePos = context.find("/NodeList/");
+  uint32_t nodeId = std::stoi(context.substr(nodePos + 10, 1));
 
-  CommandLine cmd (__FILE__);
-  cmd.AddValue ("simTime", "Total duration of the simulation", simTime);
-  cmd.AddValue ("useCa", "Whether to use carrier aggregation.", useCa);
-  cmd.Parse (argc, argv);
+  NS_LOG_DEBUG(Simulator::Now() << " Node " << nodeId 
+                << " TX: " << p->GetSize() << " bytes");
+}
 
-  // to save a template default attribute file run it like this:
-  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Save --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-simple
-  //
-  // to load a previously created default attribute file
-  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Load --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-simple
+void PhyRxTrace(std::string context, Ptr<const Packet> p) {
+  NS_LOG_DEBUG(Simulator::Now().GetSeconds() << "s: [RX] " << context 
+                << " received packet " << p->GetUid() << " (" << p->GetSize() << " bytes)");
+}
 
-  ConfigStore inputConfig;
-  inputConfig.ConfigureDefaults ();
+void DlRxErrorTrace(std::string context, Ptr<const Packet> p, double sinr) {
+  NS_LOG_DEBUG(Simulator::Now().GetSeconds() << "s: [ERROR] DL packet " 
+                << p->GetUid() << " lost (SINR: " << sinr << " dB)");
+}
 
-  // Parse again so you can override default values from the command line
-  cmd.Parse (argc, argv);
+void UlRxErrorTrace(std::string context, Ptr<const Packet> p, double sinr) {
+  NS_LOG_DEBUG(Simulator::Now().GetSeconds() << "s: [ERROR] UL packet " 
+                << p->GetUid() << " lost (SINR: " << sinr << " dB)");
+}
 
-  if (useCa)
-   {
-     Config::SetDefault ("ns3::LteHelper::UseCa", BooleanValue (useCa));
-     Config::SetDefault ("ns3::LteHelper::NumberOfComponentCarriers", UintegerValue (2));
-     Config::SetDefault ("ns3::LteHelper::EnbComponentCarrierManager", StringValue ("ns3::RrComponentCarrierManager"));
-   }
+int main (int argc, char *argv[]) {
+  // Enable detailed logging
+  LogComponentEnable("LteUePhy", LOG_LEVEL_INFO);
+  LogComponentEnable("LteUeMac", LOG_LEVEL_DEBUG);
+  LogComponentEnable("LteEnbPhy", LOG_LEVEL_INFO);
+  LogComponentEnable("LteEnbMac", LOG_LEVEL_DEBUG);
+  LogComponentEnable("LteSpectrumPhy", LOG_LEVEL_DEBUG);
+  LogComponentEnable("NBIoT", LOG_LEVEL_ALL);
 
-  Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
+  // ==================== CLI CONFIGURATION ====================
+  Time simTime = Seconds(30);
+  uint32_t numUeNodes = 1;
+  uint32_t numEnbNodes = 1;
+  double packetLossRate = 0.0;
+  bool useCa = false;
 
-  // Uncomment to enable logging
-  lteHelper->SetLogDir ("/logs");
-  lteHelper->EnableLogComponents ();
+  CommandLine cmd(__FILE__);
+  cmd.AddValue("simTime", "Simulation duration", simTime);
+  cmd.AddValue("numNodes", "Number of UE nodes", numUeNodes);
+  cmd.AddValue("numRadioTowers", "Number of eNBs", numEnbNodes);
+  cmd.AddValue("packetLossRate", "Target packet loss rate", packetLossRate);
+  cmd.AddValue("useCa", "Enable carrier aggregation", useCa);
+  cmd.Parse(argc, argv);
 
-  // Create Nodes: eNodeB and UE
+  NS_LOG_INFO("========== Simulation Configuration ==========");
+  NS_LOG_INFO("UE Nodes: " << numUeNodes);
+  NS_LOG_INFO("eNB Nodes: " << numEnbNodes);
+  NS_LOG_INFO("Packet Loss Rate: " << packetLossRate);
+  NS_LOG_INFO("Simulation Time: " << simTime.As(Time::S));
+  NS_LOG_INFO("Carrier Aggregation: " << (useCa ? "Enabled" : "Disabled"));
+  NS_LOG_INFO("==============================================");
+
+  // ==================== LTE CONFIGURATION ====================
+  Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
+  Ptr<PointToPointEpcHelper> epcHelper = CreateObject<PointToPointEpcHelper>();
+  lteHelper->SetEpcHelper(epcHelper);
+
+  lteHelper->SetSchedulerType("ns3::PfFfMacScheduler");
+
+  Config::SetDefault("ns3::LteEnbRrc::EpsBearerToRlcMapping", 
+                  EnumValue(LteEnbRrc::RLC_UM_ALWAYS));
+  Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(46.0));
+  Config::SetDefault("ns3::LteUePhy::NoiseFigure", DoubleValue(9.0));
+
+  if (packetLossRate > 0) {
+    NS_LOG_INFO("Configuring packet loss model (BER: " << packetLossRate << ")");
+    Config::SetDefault("ns3::LteAmc::Ber", DoubleValue(packetLossRate));
+    Config::SetDefault("ns3::LteSpectrumPhy::CtrlErrorModelEnabled", BooleanValue(true));
+    Config::SetDefault("ns3::LteSpectrumPhy::DataErrorModelEnabled", BooleanValue(true));
+  }
+
+  if (useCa) {
+    NS_LOG_INFO("Configuring carrier aggregation");
+    Config::SetDefault("ns3::LteHelper::UseCa", BooleanValue(true));
+    Config::SetDefault("ns3::LteHelper::NumberOfComponentCarriers", UintegerValue(2));
+    Config::SetDefault("ns3::LteHelper::EnbComponentCarrierManager", 
+                      StringValue("ns3::RrComponentCarrierManager"));
+  }
+
+  // ==================== NODE CREATION ====================
+  NS_LOG_INFO("Creating " << numEnbNodes << " eNB node(s)");
   NodeContainer enbNodes;
+  enbNodes.Create(numEnbNodes);
+
+  NS_LOG_INFO("Creating " << numUeNodes << " UE node(s)");
   NodeContainer ueNodes;
-  enbNodes.Create (1);
-  ueNodes.Create (1);
+  ueNodes.Create(numUeNodes);
 
-  // Create Devices and install them in the Nodes (eNB and UE)
-  NetDeviceContainer enbDevs;
-  NetDeviceContainer ueDevs;
-  // Default scheduler is PF, uncomment to use RR
-  //lteHelper->SetSchedulerType ("ns3::RrFfMacScheduler");
-
-  // Energy source configuration
-  BasicEnergySourceHelper basicEnergySourceHelper;
-  basicEnergySourceHelper.Set("BasicEnergySourceInitialEnergyJ", DoubleValue(10000.0)); // Initial energy
-
-  // Install energy sources on all nodes
-  EnergySourceContainer energySources;
-  energySources.Add(basicEnergySourceHelper.Install(enbNodes));
-  energySources.Add(basicEnergySourceHelper.Install(ueNodes));
-
-  // Optional: Similar configuration for eNB energy models if available
-
-  // Install Mobility Model
+  // ==================== MOBILITY ====================
   MobilityHelper mobility;
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (enbNodes);
-  BuildingsHelper::Install (enbNodes);
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (ueNodes);
-  BuildingsHelper::Install (ueNodes);
+  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
 
-  enbDevs = lteHelper->InstallEnbDevice (enbNodes);
-  ueDevs = lteHelper->InstallUeDevice (ueNodes);
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+  positionAlloc->Add(Vector(0, 0, 30));    // eNB position
+  positionAlloc->Add(Vector(50, 0, 1.5));  // UE position (50m from eNB)
+  mobility.SetPositionAllocator(positionAlloc);
 
-  // Attach a UE to a eNB
-  lteHelper->Attach (ueDevs, enbDevs.Get (0));
+  NS_LOG_INFO("Installing mobility models");
+  mobility.Install(enbNodes);
+  mobility.Install(ueNodes);
+  BuildingsHelper::Install(enbNodes);
+  BuildingsHelper::Install(ueNodes);
 
-  // Activate a data radio bearer
-  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-  EpsBearer bearer (q);
-  lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
-  lteHelper->EnableTraces ();
+  // ==================== NETWORK SETUP ====================
+  NS_LOG_INFO("Installing LTE devices");
+  NetDeviceContainer enbDevs = lteHelper->InstallEnbDevice(enbNodes);
+  NetDeviceContainer ueDevs = lteHelper->InstallUeDevice(ueNodes);
 
-  Simulator::Stop (simTime);
+  // ==================== TRACE CONNECTIONS ====================
+  NS_LOG_INFO("Connecting tracing callbacks");
 
-  // Connect traces to energy sources
-  for (EnergySourceContainer::Iterator it = energySources.Begin(); it != energySources.End(); ++it) {
-      Ptr<BasicEnergySource> source = DynamicCast<BasicEnergySource>(*it);
-      source->TraceConnectWithoutContext("RemainingEnergy", MakeCallback(&EnergyConsumptionCallback));
+  // Connect PHY traces for all devices
+  for (uint32_t i = 0; i < ueDevs.GetN(); ++i) {
+      Ptr<LteUePhy> uePhy = ueDevs.Get(i)->GetObject<LteUeNetDevice>()->GetPhy();
+      
+      // UE transmission/reception
+      uePhy->TraceConnectWithoutContext("Tx", MakeCallback(&PhyTxTrace));
+      uePhy->TraceConnectWithoutContext("Rx", MakeCallback(&PhyRxTrace));
   }
 
-  std::cout << "Test";
-  Simulator::Run ();
-
-  // GtkConfigStore config;
-  // config.ConfigureAttributes ();
-  //
-  double totalEnergyConsumed = 0.0;
-  for (EnergySourceContainer::Iterator it = energySources.Begin(); it != energySources.End(); ++it) {
-      Ptr<BasicEnergySource> source = DynamicCast<BasicEnergySource>(*it);
-      totalEnergyConsumed += (10000.0 - source->GetRemainingEnergy()); // Initial energy was 10,000 J
+  for (uint32_t i = 0; i < enbDevs.GetN(); ++i) {
+      Ptr<LteEnbPhy> enbPhy = enbDevs.Get(i)->GetObject<LteEnbNetDevice>()->GetPhy();
+      
+      // eNB transmission/reception
+      enbPhy->TraceConnectWithoutContext("Tx", MakeCallback(&PhyTxTrace));
+      enbPhy->TraceConnectWithoutContext("Rx", MakeCallback(&PhyRxTrace));
   }
-  std::cout << "Total energy consumed: " << totalEnergyConsumed << " J";
 
-  Simulator::Destroy ();
+  // ==================== INTERNET STACK ====================
+  NS_LOG_INFO("Installing internet stack on UEs");
+  InternetStackHelper internet;
+  internet.Install(ueNodes);
+
+  // ==================== EPC SETUP ====================
+  NS_LOG_INFO("Setting up EPC and remote host");
+  Ptr<Node> pgw = epcHelper->GetPgwNode();
+
+  // Create remote host
+  NodeContainer remoteHostContainer;
+  remoteHostContainer.Create(1);
+  Ptr<Node> remoteHost = remoteHostContainer.Get(0);
+  InternetStackHelper internetRemote;
+  internetRemote.Install(remoteHostContainer);
+
+  // Create P2P link between PGW and remote host
+  PointToPointHelper p2ph;
+  p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
+  p2ph.SetDeviceAttribute("Mtu", UintegerValue(1500));
+  p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+  
+  NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
+  Ipv4AddressHelper ipv4h;
+  ipv4h.SetBase("1.0.0.0", "255.0.0.0");
+  Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign(internetDevices);
+
+  // Routing setup
+  Ipv4StaticRoutingHelper ipv4RoutingHelper;
+  Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting(
+    remoteHost->GetObject<Ipv4>());
+  remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
+
+  // Assign UE IP addresses
+  Ipv4InterfaceContainer ueIpIface = epcHelper->AssignUeIpv4Address(ueDevs);
+
+  // ==================== ENERGY SETUP ====================
+  NS_LOG_INFO("Configuring energy sources");
+  BasicEnergySourceHelper energySourceHelper;
+  energySourceHelper.Set("BasicEnergySourceInitialEnergyJ", DoubleValue(10000.0));
+  
+  EnergySourceContainer energySources;
+  energySources.Add(energySourceHelper.Install(enbNodes));
+  energySources.Add(energySourceHelper.Install(ueNodes));
+
+  // Connect energy traces
+  for (EnergySourceContainer::Iterator it = energySources.Begin(); 
+       it != energySources.End(); ++it) {
+    DynamicCast<BasicEnergySource>(*it)->TraceConnectWithoutContext(
+      "RemainingEnergy", MakeCallback(&EnergyConsumptionCallback));
+  }
+
+  // ==================== NETWORK ATTACHMENT ====================
+  NS_LOG_INFO("Attaching UEs to base stations");
+  lteHelper->Attach(ueDevs, enbDevs.Get(0));
+
+  // ==================== BEARER ACTIVATION ====================
+  // NS_LOG_INFO("Activating EPS bearers");
+  // EpsBearer::Qci q = EpsBearer::NGBR_LOW_LAT_EMBB;  // Use NB-IoT specific QoS
+  // EpsBearer bearer(q);
+  // lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
+
+  // ==================== TRAFFIC SETUP ====================
+  NS_LOG_INFO("Setting up applications");
+
+  // UL Traffic from UE to remote host
+  uint16_t ulPort = 5000;
+  double interPacketInterval = 60.0;
+
+  ApplicationContainer clientApps;
+  ApplicationContainer serverApps;
+
+  // UDP Server on remote host
+  PacketSinkHelper ulPacketSinkHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), ulPort));
+  serverApps.Add(ulPacketSinkHelper.Install(remoteHost));
+
+  // UDP Client on UE
+  UdpClientHelper ulClient(remoteHost->GetObject<Ipv4>()->GetAddress(1, 0).GetLocal(), ulPort);
+  ulClient.SetAttribute("MaxPackets", UintegerValue(UINT32_MAX));
+  ulClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+  ulClient.SetAttribute("PacketSize", UintegerValue(200));
+  clientApps.Add(ulClient.Install(ueNodes.Get(0)));
+
+  // Start applications
+  serverApps.Start(Seconds(0.5));
+  clientApps.Start(Seconds(1.0));
+  serverApps.Stop(simTime - Seconds(1));
+  clientApps.Stop(simTime - Seconds(1));
+
+  // Enable LTE traces
+  lteHelper->EnableTraces();
+
+  // ==================== SIMULATION CONTROL ====================
+  Simulator::Stop(simTime);
+  NS_LOG_INFO("Starting simulation...");
+  Simulator::Run();
+  
+  NS_LOG_INFO("Simulation completed");
+  Simulator::Destroy();
+
   return 0;
 }
